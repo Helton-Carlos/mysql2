1bee311c5fab1e4f55320494b2727fc6f4c96c86
Helton <heltoncarlospa@hotmail.com>
2022-03-04 22:19:31 -0300

    start mysql post

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
-const db = require("./models/db");
+const User = require("./models/User");
+app.use(express.json());
+
 app.get("/", async (req, res) => {
   res.send("Pagina home");
 });
+
 app.post("/cadastrar", async (req, res) => {
-    res.send("Pagina cadastrar");
-  });
-app.listen(3000, () => {
+    await User.create(req.body).then(() => {
+      console.log("Enviado com sucesso");
+    })
+    .catch(() => {
+      console.log("Erro!");
+    });
+  res.send("Recebido");
+});
+
+app.listen(3001, () => {
   console.log("Acesso ao Servidor");
 });
